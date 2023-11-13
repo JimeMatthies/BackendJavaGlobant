7aaff04eeec608b206993ba4bc2844b4f669557c
Jime Matthies <jimematthies@gmail.com>
2023-11-12 22:10:09 -0300

    Backend con Java - Globant

---
+++ Guia 13 - Manejo de Excepciones/Encuentro 92 a 95 - Manejo de Excepciones/Guia13EjercicioIntegrador/src/guia13ejerciciointegrador/Guia13EjercicioIntegrador.java
@@ -0,0 +1,21 @@
+/*
+ARMADURA IRON MAN:
+J.A.R.V.I.S. es una inteligencia artificial desarrollada por Tony Stark. Está programado para hablar
+con voz masculina y acento británico. Actualmente se encarga de todo lo relacionado con la
+información doméstica de su casa, desde los sistemas de calefacción y refrigeración hasta los Hot
+Rod que Stark tiene en su garage.
+Tony Stark quiere adaptar a J.A.R.V.I.S. para que lo asista en el uso de sus armaduras, por lo tanto,
+serás el responsable de llevar adelante algunas de estas tareas.
+El objetivo de JARVIS es que analice intensivamente toda la información de la armadura y del
+entorno y en base a esto tome decisiones inteligentes.
+@author JimeM
+ */
+package guia13ejerciciointegrador;
+
+public class Guia13EjercicioIntegrador {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
