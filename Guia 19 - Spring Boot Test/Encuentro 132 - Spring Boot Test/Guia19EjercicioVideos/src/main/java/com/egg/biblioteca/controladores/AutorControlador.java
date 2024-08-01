6ff3c88d14311347a4add04a09875b46e516bb23
Jime Matthies <jimematthies@gmail.com>
2024-08-01 12:00:41 -0400

    Backend con Java - Globant

--- Guia 19 - Spring Boot Test/Encuentro 132 - Spring Boot Test/Guia19EjercicioVideos/src/main/java/com/egg/biblioteca/controladores/AutorControlador.java
+++ Guia 19 - Spring Boot Test/Encuentro 132 - Spring Boot Test/Guia19EjercicioVideos/src/main/java/com/egg/biblioteca/controladores/AutorControlador.java
@@ -1,7 +1,8 @@
 /*
 Guia 18 = Aplicación Biblioteca Spring API y Test
 @author JimeM
- */
+*/
+
 package com.egg.biblioteca.controladores;
 
 import com.egg.biblioteca.entidades.Autor;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/autores") //localhost:8080/api/autor
+@RequestMapping("/api/autores") // localhost:8080/api/autor
 public class AutorControlador {
 
     @Autowired
@@ -32,10 +33,10 @@ public class AutorControlador {
     public List<Autor> getAutores() {
         return autorServicio.getAutores();
     }
-    
+
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public Autor saveAutor(@RequestBody Autor autor){
+    public Autor saveAutor(@RequestBody Autor autor) {
         return autorServicio.saveAutor(autor);
     }
 
@@ -44,18 +45,18 @@ public class AutorControlador {
     public Optional<Autor> getAutorByID(@PathVariable("id") String id) {
         return autorServicio.getAutorById(id);
     }
-    
+
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Autor updateAutorById(@RequestBody Autor autor, @PathVariable("id") String id){
+    public Autor updateAutorById(@RequestBody Autor autor, @PathVariable("id") String id) {
         return autorServicio.updateAutorById(autor, id);
     }
-    
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deleteAutorById(@PathVariable("id") String id){
-        boolean ok = autorServicio.deleteAutorById(id);  
-        if(ok){
+    public String deleteAutorById(@PathVariable("id") String id) {
+        boolean ok = autorServicio.deleteAutorById(id);
+        if (ok) {
             return "Autor con id " + id + " eliminado";
         } else {
             return "Error, no se pudo eliminar el autor con el id " + id;
